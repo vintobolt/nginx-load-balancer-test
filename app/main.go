bceff10199d145fe663a5e91e734198d3ad55ab3
Oleksandr Zatserklianyi <vintobolt@protonmail.com>
2022-07-21 23:40:09 +0000

    fixed ipaddr in nginx.conf

--- app/main.go
+++ app/main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func health(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, http.StatusText(200))
+	w.Write([]byte(http.StatusText(200)))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("X-Server", "1")
+	w.Write([]byte("<h1>Hello World</h1>"))
 }
 
 func main() {
-	http.HandleFunc("/health", health)
-	err := http.ListenAndServe(":5000", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", health)
+	mux.HandleFunc("/", indexHandler)
+	err := http.ListenAndServe(":5000", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
